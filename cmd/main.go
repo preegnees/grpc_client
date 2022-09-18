8891b1ea43902d42bee3994749417e3e4c3f391b
preegnees <mrraziapov13@gmail.com>
2022-09-18 15:43:29 +0300

    Добавил запуск прокси

--- cmd/main.go
+++ cmd/main.go
@@ -1,19 +1,23 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"io"
 	"log"
 	"os"
+	"strings"
 
-	c "streaming/pkg/configuration"
+	cli "streaming/pkg/cli"
+	cnf "streaming/pkg/configuration"
 	m "streaming/pkg/models"
-	ltcp "streaming/pkg/listener"
+	p "streaming/pkg/proxy"
 
 	logrus "github.com/sirupsen/logrus"
 )
 
 func main() {
-	
+
 	if err := run(); err != nil {
 		log.Fatal(err)
 	}
@@ -27,21 +31,10 @@ func run() error {
 	}
 	log.Println(conf)
 
-
-	
-
-
-	err = listen(conf, logrus.New())
+	err = runProxy(*conf, logrus.New())
 	if err != nil {
 		return err
 	}
-
-	return nil
-}
-
-func listen(conf *m.Config, log *logrus.Logger) error {
-	
-	listener := ltcp.New(log)
 
 	return nil
 }
@@ -50,12 +43,74 @@ func readConf(log *logrus.Logger) (*m.Config, error) {
 	if len(os.Args[1:]) != 1 {
 		return nil, fmt.Errorf("Args is empty")
 	}
-	
-	configuration := c.New(log)
+
+	configuration := cnf.New(log)
 	conf, err := configuration.Get(os.Args[1:][0])
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	return conf, nil
+}
+
+func runProxy(conf m.Config, log *logrus.Logger) error {
+
+	proxies := make([]p.Proxy, 0)
+
+	errCh := make(chan error)
+	ctx, cancel := context.WithCancel(context.Background())
+
+	// служебный канал потом реализовать
+
+	for _, v := range conf.Channels {
+		prFromRpcClient, pwToProxyClient := io.Pipe()
+		prFromProxyServer, pwToRpcClient := io.Pipe()
+		portRead := strings.Split(v.Ports, " ")[0]
+		portWrite := strings.Split(v.Ports, " ")[1]
+
+		proxy := p.New(
+			log,
+			portWrite,
+			portRead,
+			prFromRpcClient,
+			pwToRpcClient,
+			conf.Buffer,
+		)
+
+		proxies = append(proxies, proxy)
+
+		proxy.Run(ctx, errCh)
+
+		go func(v m.Channel, eCh chan error, prFromProxyServer *io.PipeReader, pwToProxyClient *io.PipeWriter) {
+			c := cli.New()
+			err := c.Run(m.ClientConf{
+				Addr:              conf.Server,
+				RequestTimeout:    15,
+				KeepaliveInterval: 600,
+				Reconnect:         true,
+				ReconnectTimeout:  15,
+				IdChannel:         v.IdChannel,
+				Name:              conf.Name,
+				AllowedNames:      fmt.Sprintf("%s,%s", v.AllowedNames, conf.Name),
+				Reader:            prFromProxyServer,
+				Writer:            pwToProxyClient,
+			})
+			if err != nil {
+				if eCh != nil {
+					eCh <- err
+					return
+				}
+				return
+			}
+			return
+		}(v, errCh, prFromProxyServer, pwToProxyClient)
+	}
+
+	select {
+	case err := <-errCh:
+		close(errCh)
+		errCh = nil
+		cancel()
+		return err
+	}
 }
